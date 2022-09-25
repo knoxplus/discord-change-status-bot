24537edc909cd304bb63472f5027a432a6c6787f
knoxplus <hossein.j.a.hja@gmail.com>
2022-09-25 19:38:43 +0330

    final

--- src/bot.js
+++ src/bot.js
@@ -2,13 +2,15 @@ const { Client, GatewayIntentBits , ActivityType } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 //set your token here
+const Yourtoken = '';
+
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
   //change 'your stats' here 
-  client.user.setPresence({ activities: [{ name: 'NFTs (soon)', type: ActivityType.Watching }], status: 'online'});
+  client.user.setPresence({ activities: [{ name: 'your stats', type: ActivityType.Watching }], status: 'online'});
 });
 
 
-client.login('MTAyMzEyOTUzMjU0MDEyOTMyMA.GZqOUV.fQnJrsghSrV7fTmFvnDSlRX6p7Dsijn0wHPgeY');
+client.login(Yourtoken);
